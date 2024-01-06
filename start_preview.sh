a1d22ac1217d5f4480596040f4a3fa37b2a14c80
zhengyiming <realzhengyiming@outlook.com>
2024-01-06 23:04:35 +0800

    add cmd

--- start_preview.sh
+++ start_preview.sh
@@ -1,4 +1,17 @@
 cd YiMingBlog
 npm run docs:build
+
+# 查找占用 80 端口的进程
+PID=$(lsof -t -i :80)
+
+if [ -z "$PID" ]; then
+  echo "没有找到在 80 端口运行的进程。"
+else
+  # 终止找到的进程
+  echo "找到运行在 80 端口的进程 PID: $PID"
+  kill -9 $PID
+  echo "进程已终止。"
+fi
+
 nohup npm run docs:preview > bloglog.txt 2>&1 &
 

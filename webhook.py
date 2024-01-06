050e0cc338e03f9d75217aff29936ee60955960c
zhengyiming <realzhengyiming@outlook.com>
2024-01-06 11:00:23 +0800

    test

--- webhook.py
+++ webhook.py
@@ -1,7 +1,32 @@
-from flask import Flask, request
+import hmac
+import hashlib
+import os
+
+from flask import Flask, request, abort
 import subprocess
 
 app = Flask(__name__)
+
+
+@app.before_request
+def verify_signature():
+    signature = request.headers.get('X-Hub-Signature-256')
+    if not signature:
+        abort(400, 'No signature found')
+
+    secret = os.getenv("HOOKSERCERT")  # 在这里替换为你在 GitHub 上设置的密钥
+    payload = request.get_data()
+
+    # 根据签名算法类型计算签名
+    algorithm, signature = signature.split('=')
+    if algorithm == 'sha256':
+        calculated_signature = hmac.new(secret.encode(), payload, hashlib.sha256).hexdigest()
+    else:
+        abort(400, 'Invalid signature algorithm')
+
+    # 比较计算得到的签名和传入的签名是否相等
+    if not hmac.compare_digest(calculated_signature, signature):
+        abort(401, 'Signature verification failed')
 
 
 @app.route('/', methods=['GET'])
@@ -27,6 +52,8 @@ def handle_webhook():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
-
-
+    preview_secret = os.getenv("HOOKSERCERT", "")  # 在这里替换为你在 GitHub 上设置的密钥
+    if not preview_secret:
+        print("please export HOOKSECRET=  first！")
+    else:
+        app.run(host='0.0.0.0', port=3000)

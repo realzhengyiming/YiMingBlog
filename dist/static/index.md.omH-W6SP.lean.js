9e69cb0ae3d0b1bbb14b226eb85451ade3dedd03
zhengyiming <realzhengyiming@outlook.com>
2024-01-28 00:24:35 +0800

    public blog

--- dist/static/index.md.omH-W6SP.lean.js
+++ dist/static/index.md.omH-W6SP.lean.js
@@ -1,11 +1,1 @@
-import {_ as e, c as t, o as a} from "./chunks/framework.wSs3aZfZ.js";
-
-const f = JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"YiMing","text":"记录技术与生活","tagline":"code for fun","image":{"src":"/static/icons8-猫.gif","alt":"Yiming"},"actions":[{"theme":"brand","text":"最新文章","link":"/post"}]},"features":[{"title":"爬虫","details":"数据获取抓取技术交流"},{"title":"机器学习","details":"机器学习相关"},{"title":"深度学习","details":"深度学习相关"}],"footer":true},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),
-    i = {name: "index.md"};
-
-function n(o, r, s, c, d, l) {
-    return a(), t("div")
-}
-
-const p = e(i, [["render", n]]);
-export {f as __pageData, p as default};
+import{_ as e,c as t,o as a}from"./chunks/framework.wSs3aZfZ.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"YiMing","text":"记录技术与生活","tagline":"code for fun","image":{"src":"/static/icons8-猫.gif","alt":"Yiming"},"actions":[{"theme":"brand","text":"最新文章","link":"/post"}]},"features":[{"title":"爬虫","details":"数据获取抓取技术交流"},{"title":"机器学习","details":"机器学习相关"},{"title":"深度学习","details":"深度学习相关"}],"footer":true},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),i={name:"index.md"};function n(o,r,s,c,d,l){return a(),t("div")}const p=e(i,[["render",n]]);export{f as __pageData,p as default};

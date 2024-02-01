167f92f93ea70066d791df3ce25183e663dbd061
zhengyiming <realzhengyiming@outlook.com>
2024-02-02 00:55:48 +0800

    update blog content

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import { h } from 'vue'
 import { VPBTheme } from '@jcamp/vitepress-blog-theme'
 import './style.css'
 import CustomBlogHeader from './CustomBlogHeader.vue'
-import MyDiv from './my_div.vue'
+// import MyDiv from './my_div.vue'
 import MyDiv2 from './change_face.vue'
 
 export default {
@@ -16,8 +16,8 @@ export default {
   enhanceApp({ app, router, siteData }) {
     VPBTheme.enhanceApp({ app, router, siteData })
     app.component('CustomBlogHeader', CustomBlogHeader)
-    app.component("MyDiv", MyDiv)
-    app.component("MyDiv", MyDiv2)
+    // app.component("MyDiv", MyDiv)
+    app.component("MyDiv2", MyDiv2)
   },
 }
 
